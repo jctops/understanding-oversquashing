72fec21b099678538a409e8314b056d9acb07118
Jakub Bober <jakub.bober18@imperial.ac.uk>
2022-05-16 00:37:25 +0100

    Change neighbor set of y

--- gdl/src/gdl/curvature/numba.py
+++ gdl/src/gdl/curvature/numba.py
@@ -204,10 +204,10 @@ def sdrf(
 
         if is_undirected:
             x_neighbors = list(G.neighbors(x)) + [x]
-            y_neighbors = list(G.neighbors(y)) + [x]
+            y_neighbors = list(G.neighbors(y)) + [y]
         else:
             x_neighbors = list(G.successors(x)) + [x]
-            y_neighbors = list(G.predecessors(y)) + [x]
+            y_neighbors = list(G.predecessors(y)) + [y]
         candidates = []
         for i in x_neighbors:
             for j in y_neighbors:
